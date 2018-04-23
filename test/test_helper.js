c62a0c7b7df93d0628f5ddfaf9656ead046206ce
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-04-23 18:14:07 +0400

    test environment setup : build renderComponent helper : done partially, some tests do pass

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,5 +1,13 @@
 import jsdom from 'jsdom';
 import jquery from 'jquery';
+import TestUtils from 'react-addons-test-utils';
+import ReactDOM from 'react-dom';
+import { expect } from 'chai';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from '../src/reducers';
+
 
 // Set up testing environment to run like a browser in the command line
 
@@ -12,7 +20,21 @@ const $ = jquery(global.window);
 
 // build 'renderComponent' helper that should render a given react class
 
+function renderComponent(ComponentClass) {
+	const componentInstance = TestUtils.renderIntoDocument(
+		<Provider store={createStore(reducers)}>
+			<ComponentClass />
+		</Provider>
+	);
+	// it holds the reference to rendered version of our component
+
+	// ReactDOM.findDOMNode(componentInstance)  // this produces HTML
+	return $(ReactDOM.findDOMNode(componentInstance));
+
+
+}
+
 // Build helper for simulating events
 
 // Set up chai-jquery
-
+export { renderComponent, expect };
