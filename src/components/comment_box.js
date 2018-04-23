efdffd78284f9dc85aef084d77ec7501dff9b0ae
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-04-23 14:55:47 +0400

    aded comments reducer and tested it completely

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-export default class CommentBox extends Component {
+class CommentBox extends Component {
 	constructor(props) {
 		super(props);
 
@@ -13,7 +15,10 @@ export default class CommentBox extends Component {
 
 	handleSubmit(event) {
 		event.preventDefault();
+
+		this.props.saveComment(this.state.comment);
 		this.setState({ comment: '' });
+
 	}
 
 	render() {
@@ -27,3 +32,5 @@ export default class CommentBox extends Component {
 		)
 	}
 }
+
+export default connect(null, actions)(CommentBox);
